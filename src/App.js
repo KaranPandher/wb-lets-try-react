e02a77cc10dc3eaf87f4a978bcb527a72b64a225
Karan Pandher <karanpandher@live.ca>
2020-04-07 14:34:16 -0600

    First React App

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1> Hello World! </h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p> I am adding some text! </p>
         <a
           className="App-link"
           href="https://reactjs.org"
